7ab05e2ae62cfc494650637bcf83a30232c9788b
Assad Usman <assadusman@assads-air.home>
2024-10-31 22:48:11 -0400

    changed port to 8082

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());  // To parse JSON request bodies
 // Use the datasets route for /api
 app.use('/api', datasetsRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
