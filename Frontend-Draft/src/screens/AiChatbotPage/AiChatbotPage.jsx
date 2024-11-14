9e76b1bbe2db23292d1a657447ab24dba32d0377
Assad Usman <assadusman@Assads-MacBook-Air.local>
2024-11-13 21:03:13 -0500

    fixed import issue

--- Frontend-Draft/src/screens/AiChatbotPage/AiChatbotPage.jsx
+++ Frontend-Draft/src/screens/AiChatbotPage/AiChatbotPage.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom';
 import "./style.css";
 
 export const AiChatbotPage = () => {
