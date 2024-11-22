56657038d0d315862a252555d61cba14f135d35b
modaserMoj <54120999+modaserMoj@users.noreply.github.com>
2024-11-21 21:25:47 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 
 // Import the routes
 const datasetsRoute = require('./routes/datasets');
-const chatRoute = require('./routes/chatbot'); // Import the chatbot route
 
 const app = express();
 app.use(cors());
@@ -12,7 +11,6 @@ app.options('*', cors());  // Enable pre-flight across-the-board
 
 // Use the routes
 app.use('/api', datasetsRoute); // Route for datasets
-app.use('/api/chat', chatRoute);         // Route for chatbot
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
