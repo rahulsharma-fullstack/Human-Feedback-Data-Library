4bc00d71ed5a10a8a3ded4747a0e1b17e09dc70d
Assad Usman <assadusman@Assads-MacBook-Air.local>
2024-11-28 03:36:32 -0500

    added resizing for smaller mobile screens

--- Frontend-Draft/src/screens/GoogleFormPage/GoogleFormPage.jsx
+++ Frontend-Draft/src/screens/GoogleFormPage/GoogleFormPage.jsx
@@ -7,7 +7,7 @@ const GoogleFormPage = () => {
   return (
     <div className="google-form-page container-fluid">
       {/* Page Heading */}
-      <div className="group-2 header-instance">
+      {/* <div className="group-2 header-instance">
         <div className="navbar">
           <Link className="text-wrapper-21" to="/ai-chatbot-page">
             Chatbot
@@ -29,7 +29,60 @@ const GoogleFormPage = () => {
           </Link>
         </div>
         <img className="logo" alt="Logo" src="/img/logo.png" />
-      </div>
+      </div> */}
+
+      <nav className="navbar navbar-expand-lg navbar-3">
+        <div className="container-fluid">
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarContent"
+            aria-controls="navbarContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/ai-chatbot-page">
+                  Chatbot
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/search-page">
+                  Datasets
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about-page">
+                  About Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/userguide-page">
+                  User Guide & Help
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/google-form">
+                  Submit a Dataset
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <img className="logo" alt="Logo" src="/img/logo.png" />
 
       {/* Embedded Google Form */}
       <div className="form-container dropdown-box items-frame">
